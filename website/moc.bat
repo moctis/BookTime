4eb49410f3badd4bde5712b41b0fc269ef284acc
sitthidate nanuea <sitthidate@moctis.com>
2014-07-25 23:08:36 +0700

    update phonegap command

--- website/moc.bat
+++ website/moc.bat
@@ -1,14 +1,28 @@
 @echo off
-echo %1%
-
-if %1% == build goto BUILD
-if %1% == serve goto SERVE
+if "%1%" == ""          goto HELP
+if "%1%" == "init"      goto INIT
+if "%1%" == "build"     goto BUILD
+if "%1%" == "serve"     goto SERVE
 
 :HELP
-echo moc build      to build
-echo moc serve      to serve
-pause
+
+echo.
+echo Usage: moc [command] [arguments]
+echo.
+echo Commands:
+echo    init            init
+echo    build           call pub build
+echo    serve           call pub serve --hostname=192.168.1.115
+echo.
 goto :EOF
+
+
+
+:INIT
+
+
+goto :EOF
+
 
 
 
