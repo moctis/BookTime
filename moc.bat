f58159c0163c8ce79d5b28e6e85d7bae89eb24f6
sitthidate nanuea <sitthidate@moctis.com>
2014-07-26 03:54:54 +0700

    update moc.bat

---
+++ moc.bat
@@ -0,0 +1,73 @@
+@echo off
+cd /d %~dp0
+if "%1%" == ""          goto USAGE
+if "%1%" == "help"      goto USAGE
+if "%1%" == "init"      goto INIT
+if "%1%" == "pub"       goto PUB
+if "%1%" == "phonegap"  goto PHONEGAP
+goto HELP
+
+
+
+:USAGE
+echo.
+echo Usage: moc [command] [arguments]
+echo.
+echo Commands:
+echo    pub build                pub build and copy to phonegap
+echo    pub serve                pub serve for http://192.168.1.115:8080
+echo    pub [arguments]          pub command run at ./website folder
+echo.
+echo    phonegap serve           phonegap serve for http://localhost:3000
+echo    phonegap [arguments]     phonegap command run at ./phonegap
+echo.
+echo    init                     init things that github ignored.
+echo    help                     output usage information
+echo.
+goto :EOF
+
+
+
+:PUB
+cd /d %~dp0/website
+if "%2%" == "build"  goto PUB_BUILD
+if "%2%" == "serve"  goto PUB_SERVE
+%*
+goto :EOF
+
+
+
+:PUB_BUILD
+call pub build
+if NOT ERRORLEVEL 0 goto :eof
+
+robocopy build\web ..\phonegap\www /E /NJH /NJS
+goto :EOF
+
+
+
+:PUB_SERVE
+call pub serve --hostname=192.168.1.115
+goto :EOF
+
+
+
+:PHONEGAP
+cd /d %~dp0/phonegap
+echo on
+%*
+goto :EOF
+
+
+
+:INIT
+echo : pub get
+cd /d %~dp0/website
+call pub get
+
+echo : cordova platform add android
+cd /d %~dp0/phonegap
+call cordova platform add android
+
+
+goto :EOF
