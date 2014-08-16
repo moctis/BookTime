d134c638a9b9bcfdc2285bd79fff08a2ba90f218
sitthidate nanuea <sitthidate@moctis.com>
2014-08-16 16:50:23 +0700

    update main

--- website/lib/RouteInitializer.dart
+++ website/lib/RouteInitializer.dart
@@ -4,5 +4,6 @@ void RouteInitializer(Router router, RouteViewFactory views) {
   views.configure({
     'front': ngRoute(path: "/front", view: 'views/viewFront.html', defaultRoute: true),
     'home': ngRoute(path: "/home", view: 'views/viewHome.html'),
+    'shop': ngRoute(path: "/shop", view: 'views/viewShop.html'),
   });
 }
