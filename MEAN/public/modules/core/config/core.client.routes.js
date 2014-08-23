e2332766f8dcca35b6b893079c19dbbde4092e25
sitthidate nanuea <sitthidate@moctis.com>
2014-08-24 05:21:01 +0700

    move old code

--- MEAN/public/modules/core/config/core.client.routes.js
+++ MEAN/public/modules/core/config/core.client.routes.js
@@ -12,5 +12,12 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
 		});
+
+
+	    $stateProvider.
+		state('new_page', {
+		    url: '/new_page',
+		    templateUrl: 'modules/core/views/new_page.html'
+		});
 	}
 ]);
