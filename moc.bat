e21ed019bab53301440d889b525fd88d4943d7e6
sitthidate nanuea <sitthidate@moctis.com>
2014-08-23 21:33:17 +0700

    change moc.bat

--- moc.bat
+++ moc.bat
@@ -2,14 +2,14 @@
 cd /d %~dp0
 if "%1%" == ""          goto USAGE
 if "%1%" == "help"      goto USAGE
-
+if "%1%" == "pub"       goto PUB
+if "%1%" == "init"      goto INIT
 if "%1%" == "phonegap"  goto PHONEGAP
 if "%1%" == "mean"      goto MEAN
 if "%1%" == "mongo"     goto MONGO
 goto HELP
 REM ---- not use ---
-if "%1%" == "pub"       goto PUB
-if "%1%" == "init"      goto INIT
+
 
 :USAGE
 echo.
@@ -25,15 +25,17 @@ echo    phonegap [arguments]     phonegap command run at ./phonegap
 echo.
 echo    help                     output usage information
 echo.
-goto :EOF
-REM ---- not use ---
 echo    pub build                pub build and copy to phonegap
 echo    pub serve                pub serve for http://192.168.1.115:8080
 echo    pub [arguments]          pub command run at ./website folder
 echo    init                     init things that github ignored.
+echo.
+goto :EOF
+REM ---- not use ---
+
 
 :PUB
-cd /d %~dp0/website
+cd /d %~dp0/dart
 if "%2%" == "build"  goto PUB_BUILD
 if "%2%" == "serve"  goto PUB_SERVE
 %*
