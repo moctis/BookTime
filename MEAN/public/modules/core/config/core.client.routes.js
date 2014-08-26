72e93254c8d2c976668acf3011edfae2b28565bd
sitthidate nanuea <sitthidate@moctis.com>
2014-08-26 23:42:52 +0700

    #8 moc main side bar

--- MEAN/public/modules/core/config/core.client.routes.js
+++ MEAN/public/modules/core/config/core.client.routes.js
@@ -10,7 +10,7 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		$stateProvider.
 		state('home', {
 			url: '/home',
-			templateUrl: 'modules/core/views/home.client.view.html'
+			templateUrl: 'modules/core/views/main.client.view.html'
 		});
 
 
