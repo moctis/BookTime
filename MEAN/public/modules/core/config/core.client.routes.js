d0574445a7cfd6769ce964bfaa6d4226e4334cda
sitthidate nanuea <sitthidate@moctis.com>
2014-08-31 01:36:04 +0700

    #8 update sidebar can navigate main tabbar

--- MEAN/public/modules/core/config/core.client.routes.js
+++ MEAN/public/modules/core/config/core.client.routes.js
@@ -7,15 +7,13 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		$urlRouterProvider.otherwise('/');
 
 		// Home state routing
-		$stateProvider.
-		state('home', {
+		$stateProvider.state('home', {
 			url: '/home',
 			templateUrl: 'modules/core/views/main.client.view.html'
-		});
-
-
-	    $stateProvider.
-		state('new_page', {
+		}).state('menu', {
+		    url: '/menu',
+			templateUrl: 'modules/core/views/menu.client.view.html'
+		}).state('new_page', {
 		    url: '/new_page',
 		    templateUrl: 'modules/core/views/new_page.html'
 		});
--- MEAN/public/modules/core/controllers/menu.client.controller.js
+++ MEAN/public/modules/core/controllers/menu.client.controller.js
@@ -1,29 +1,36 @@
 'use strict';
 
 
-angular.module('core').controller('MenuController', ['$scope', '$interval',
-	function ($scope, $interval) {
+angular.module('core').controller('MenuController', ['$scope', '$interval', '$rootScope',
+	function ($scope, $interval, $rootScope) {	    
+	    var ons = $rootScope.ons || {};
+	    
 	    $scope.profile = [];
 	    $scope.profile.name = 'Pony Somrattanach';
 	    $scope.profile.booked = 15;
 	    $scope.profile.favorites = 245;
 
 	    $scope.menus = [
-	        { label: 'HOME', icon: 'fa-home', page: 'modules/home/views/home.client.view.html' },
+	        { label: 'HOME', icon: 'fa-home', tabbar: 2 },
 	        { label: 'SEARCH', icon: 'fa-search', page: 'modules/home/views/home.client.view.html' },
 	        { label: 'MY BOOKING', icon: 'fa-gear', page: 'modules/home/views/home.client.view.html' },
-	        { label: 'SCHEDULE', icon: 'fa-calendar', page: 'modules/home/views/home.client.view.html' },
-	        { label: 'CHAT', icon: 'fa-comments', page: 'modules/home/views/home.client.view.html' },
-	        { label: 'NOTIFICATIONS', icon: 'fa-exclamation-circle', page: 'modules/home/views/home.client.view.html' },
+	        { label: 'SCHEDULE', icon: 'fa-calendar', tabbar: 0 },
+	        { label: 'CHAT', icon: 'fa-comments', tabbar: 1},
+	        { label: 'NOTIFICATIONS', icon: 'fa-exclamation-circle', tabbar: 3},
 	        { label: 'MY FAVORITES', icon: 'fa-exclamation-circle', page: 'modules/home/views/home.client.view.html' },
-	        { label: 'SETTINGS', icon: 'fa-gear', page: 'modules/home/views/home.client.view.html' },
+	        { label: 'SETTINGS', icon: 'fa-gear', tabbar: 4},
 	        { label: 'ABOUT BOOKTIME', icon: 'fa-gear', page: 'modules/home/views/home.client.view.html', className:'menu-about'},
 	        { label: 'SIGN OUT', icon: 'fa-gear', page: 'modules/home/views/home.client.view.html', className: 'menu-signout' }
 	    ];
 
 	    $scope.showDetail = function ($index) {
+	        //console.log(ons);
 	        var selectedMenu = $scope.menus[$index];
-	        alert(selectedMenu.label);
+	        if (selectedMenu.tabbar != undefined)
+	            ons.tabbar.setActiveTab(selectedMenu.tabbar);
+            else
+	            alert(selectedMenu.label);
+	        ons.slidingMenu.toggleMenu();
 	    };
 
 	    $scope.getClass = function($index) {
