de21087deae3031650351a6c927bbe580cc4d686
sitthidate nanuea <sitthidate@moctis.com>
2014-10-09 08:00:07 +0700

    mock service

--- MEAN/public/modules/home/controllers/home.client.controller.js
+++ MEAN/public/modules/home/controllers/home.client.controller.js
@@ -16,10 +16,12 @@ angular.module('home').controller('HomeController', ['$scope', '$rootScope',
 	        var mock = function() {
 	            return {
 	                id: ['1', '2', '3'].random(),
-	                name: ['Bangkok Shokudo----','Sushi Masa', 'Staw wery'].random(),
+	                name: ['Bangkok Shokudo','Sushi Masa', 'Staw wery'].random(),
 	                rank:[3,4,5].random(),
+	                catalog: ['Pet Care', 'Dessert/Bakery', 'Cinema'].random(),
 	                distance: ['2.4km away', '14km away', '0.4km away'].random(),
 	                booked: ['3.4k', '200', '15k'].random(),
+	                comments: ['224', '5', '11', '35'].random(),
 	                image: basepath + ['shop-1.jpg', 'shop-2.jpg'].random()
 	            };
 	        };
