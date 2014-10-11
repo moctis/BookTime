efb1970df4edaa7012fd9e25ba116ac7faf6b50d
sitthidate nanuea <sitthidate@moctis.com>
2014-10-12 01:39:07 +0700

    Mock Shop detail screen

--- MEAN/public/modules/shop/controllers/shop.client.controller.js
+++ MEAN/public/modules/shop/controllers/shop.client.controller.js
@@ -17,5 +17,30 @@ angular.module('shop').controller('ShopController', [
         $scope.getDirection = function () {
             alert('Get Direction');
         };
+
+        $scope.getData = function () {
+            var basepath = 'res/shops/1/';
+            var mock = function () {
+                return {
+                    id: ['1', '2', '3'].random(),
+                    name: ['Bangkok Shokudo', 'Sushi Masa', 'Staw wery'].random(),
+                    rank: [3, 4, 5].random(),
+                    address: ['162 Paholyothin 5 Samsennai Phyathai'].random(),
+                    address2: ['Bangkok Thailand 10400'].random(),
+                    lastBooked: ['45min ago', '10sec ago', '10days ago'].random(),
+                    catalog: ['Pet Care', 'Dessert/Bakery', 'Cinema'].random(),
+                    distance: ['2.4km away', '14km away', '0.4km away'].random(),
+                    booked: ['3.4k', '200', '15k'].random(),
+                    comments: ['224', '5', '11', '35'].random(),
+                    operationTime: ['TUE-SON 10am-11pm'].random(),
+                    image: basepath + ['main.jpg'].random(),
+                    albums: [basepath + 'a1.jpg', basepath + 'a2.jpg',basepath + 'a3.jpg',basepath + 'a4.jpg']
+                };
+            };
+
+            $scope.it = mock(); 
+        };
+
+        $scope.getData();
     }
 ]);
