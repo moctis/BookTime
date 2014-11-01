7a03389661b85d747b201307bf767318a7db8555
sitthidate nanuea <sitthidate@moctis.com>
2014-11-02 00:10:53 +0700

    Fix front page & Update Phonegap

--- phonegap/www/modules/core/config/core.client.routes.js
+++ phonegap/www/modules/core/config/core.client.routes.js
@@ -3,7 +3,7 @@
 //Setting up route
 angular.module('core').config(['$stateProvider','$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise("/main/tab/home/recommend");
+        $urlRouterProvider.otherwise("/front");
         // Core state routing
         $stateProvider.
             state('main', {
