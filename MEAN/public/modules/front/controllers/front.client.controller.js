81dad09a27df1f559bc1afe6a8e79d80f47f8820
sitthidate nanuea <sitthidate@moctis.com>
2014-12-18 09:32:14 +0700

    try to request cross origin but can't get the loggin.

--- MEAN/public/modules/front/controllers/front.client.controller.js
+++ MEAN/public/modules/front/controllers/front.client.controller.js
@@ -2,41 +2,50 @@
 
 
 angular.module('front').controller('frontController', [
-    '$scope', '$location', '$http', 'Authentication',
-    function($scope, $location, $http, Authentication) {
-      $scope.credentials = {
-        username:'',
-        password:''
-      };
-      
-      $scope.authentication = Authentication;
+  '$scope', '$location', '$http', 'Authentication',
+  function($scope, $location, $http, Authentication) {
+    $scope.credentials = {
+      username: '',
+      password: ''
+    };
+
+    $scope.authentication = Authentication;
 
 
-      // If user is signed in then redirect back home
-      if ($scope.authentication.user) $location.path('/main/tab/home/food');
+    // If user is signed in then redirect back home
+    if ($scope.authentication.user) $location.path('/main/tab/home/food');
 
-        $scope.login = function () {
-            $location.path('/main/tab/home/food');
-        };
+    $scope.login = function() {
+      $location.path('/main/tab/home/food');
+    };
 
-        $scope.carouselIndex = 0;
-        $scope.slides = [1,2,3,4];
+    $scope.carouselIndex = 0;
+    $scope.slides = [1, 2, 3, 4];
 
-        $scope.init = function() {
+    $scope.init = function() {
 
-        };
+    };
 
-        $scope.signin = function() {
-          console.log($scope.credentials);
-          $http.post('/auth/signin', $scope.credentials).success(function(response) {
-            // If successful we assign the response to the global user model
-            $scope.authentication.user = response;
+    $scope.signin = function() {
+      console.log($scope.credentials);
 
-            // And redirect to the index page
-            $location.path('/main/tab/home/food');
-          }).error(function(response) {
-            $scope.error = response.message;
-          });
-        };
-    }
+      //ref: https://gist.github.com/kkurni/4618210
+
+      var $apihost = 'http://localhost:3003';
+      $http.post($apihost + '/auth/signin', $scope.credentials, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;'
+        }
+      }).success(function(response) {
+        // If successful we assign the response to the global user model
+        $scope.authentication.user = response;
+
+        // And redirect to the index page
+        $location.path('/main/tab/home/food');
+      }).error(function(response) {
+        $scope.error = response.message;
+      });
+    };
+  }
 ]);
--- MEAN/public/modules/users/controllers/authentication.client.controller.js
+++ MEAN/public/modules/users/controllers/authentication.client.controller.js
@@ -1,41 +1,42 @@
 'use strict';
 
 angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
-	function($scope, $http, $location, Authentication) {
-		$scope.authentication = Authentication;
+  function($scope, $http, $location, Authentication) {
+    $scope.authentication = Authentication;
 
-		// If user is signed in then redirect back home
-		if ($scope.authentication.user) $location.path('/');
+    // If user is signed in then redirect back home
+    if ($scope.authentication.user) $location.path('/');
 
-		$scope.signup = function() {
-			$http.post('/auth/signup', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
+    $scope.signup = function() {
+      $http.post('/auth/signup', $scope.credentials).success(function(response) {
+        // If successful we assign the response to the global user model
+        $scope.authentication.user = response;
 
-				// And redirect to the index page
-				$location.path('/');
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
-		};
+        // And redirect to the index page
+        $location.path('/');
+      }).error(function(response) {
+        $scope.error = response.message;
+      });
+    };
 
-		$scope.signin = function() {
-			$http.post('/auth/signin', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
+    $scope.signin = function() {
+      var $apihost = 'ttet/';
+      $http.post($apihost + '/auth/signin', $scope.credentials).success(function(response) {
+        // If successful we assign the response to the global user model
+        $scope.authentication.user = response;
 
-				// And redirect to the index page
-				$location.path('/');
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
-		};
-		$scope.signout = function() {
-			/*$http.post('/auth/singout').success(function(response) {
+        // And redirect to the index page
+        $location.path('/');
+      }).error(function(response) {
+        $scope.error = response.message;
+      });
+    };
+    $scope.signout = function() {
+      /*$http.post('/auth/singout').success(function(response) {
 				$location.path('/');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});*/
-		};
-	}
+    };
+  }
 ]);
