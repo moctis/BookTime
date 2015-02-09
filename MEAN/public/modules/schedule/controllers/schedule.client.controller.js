4eb8ccffb380615c5898aa77fa068bb21ee66d2c
sitthidate nanuea <sitthidate@moctis.com>
2015-02-09 20:58:22 +0700

    * reformat

--- MEAN/public/modules/schedule/controllers/schedule.client.controller.js
+++ MEAN/public/modules/schedule/controllers/schedule.client.controller.js
@@ -2,72 +2,115 @@
 
 angular.module('schedules').controller('ScheduleController', [
   '$scope', '$timeout', '$locale',
-  function($scope, $timeout, $locale, uiCalendarConfig) {  	
-  	var date = new Date();
+  function($scope, $timeout, $locale, uiCalendarConfig) {
+    var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
-    var y = date.getFullYear();  	    
+    var y = date.getFullYear();
 
     $scope.changeTo = 'English';
     /* event source that pulls from google.com */
     $scope.eventSource = {
-            url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
-            className: 'gcal-event',           // an option!
-            currentTimezone: 'Bangkok' // an option!
+      url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
+      className: 'gcal-event', // an option!
+      currentTimezone: 'Bangkok' // an option!
     };
 
     /* event source that contains custom events on the scope */
-    $scope.events = [
-      {title: 'All Day Event',start: new Date(y, m, 1)},
-      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'},
-      {title: 'valentine', start: new Date(y, 1, 14),end: new Date(y, 1, 14)}
-    ];
+    $scope.events = [{
+      title: 'All Day Event',
+      start: new Date(y, m, 1)
+    }, {
+      title: 'Long Event',
+      start: new Date(y, m, d - 5),
+      end: new Date(y, m, d - 2)
+    }, {
+      id: 999,
+      title: 'Repeating Event',
+      start: new Date(y, m, d - 3, 16, 0),
+      allDay: false
+    }, {
+      id: 999,
+      title: 'Repeating Event',
+      start: new Date(y, m, d + 4, 16, 0),
+      allDay: false
+    }, {
+      title: 'Birthday Party',
+      start: new Date(y, m, d + 1, 19, 0),
+      end: new Date(y, m, d + 1, 22, 30),
+      allDay: false
+    }, {
+      title: 'Click for Google',
+      start: new Date(y, m, 28),
+      end: new Date(y, m, 29),
+      url: 'http://google.com/'
+    }, {
+      title: 'valentine',
+      start: new Date(y, 1, 14),
+      end: new Date(y, 1, 14)
+    }];
 
     /* event source that calls a function on every view switch */
-    $scope.eventsF = function (start, end, timezone, callback) {
+    $scope.eventsF = function(start, end, timezone, callback) {
       var s = new Date(start).getTime() / 1000;
       var e = new Date(end).getTime() / 1000;
       var m = new Date(start).getMonth();
-      var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
+      var events = [{
+        title: 'Feed Me ' + m,
+        start: s + (50000),
+        end: s + (100000),
+        allDay: false,
+        className: ['customFeed']
+      }];
       callback(events);
     };
 
     $scope.calEventsExt = {
-       color: '#f00',
-       textColor: 'yellow',
-       events: [ 
-          {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-        ]
+      color: '#f00',
+      textColor: 'yellow',
+      events: [{
+        type: 'party',
+        title: 'Lunch',
+        start: new Date(y, m, d, 12, 0),
+        end: new Date(y, m, d, 14, 0),
+        allDay: false
+      }, {
+        type: 'party',
+        title: 'Lunch 2',
+        start: new Date(y, m, d, 12, 0),
+        end: new Date(y, m, d, 14, 0),
+        allDay: false
+      }, {
+        type: 'party',
+        title: 'Click for Google',
+        start: new Date(y, m, 28),
+        end: new Date(y, m, 29),
+        url: 'http://google.com/'
+      }]
     };
     /* alert on eventClick */
-    $scope.alertOnEventClick = function( date, jsEvent, view){
-        //$scope.alertMessage = (date.title + ' was clicked ');
-        alert(date.title + ' was clicked ');
+    $scope.alertOnEventClick = function(date, jsEvent, view) {
+      //$scope.alertMessage = (date.title + ' was clicked ');
+      alert(date.title + ' was clicked ');
     };
     /* alert on Drop */
-     $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
-       $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
+    $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view) {
+      $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
     };
     /* alert on Resize */
-    $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
-       $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
+    $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view) {
+      $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
     };
     /* add and removes an event source of choice */
-    $scope.addRemoveEventSource = function(sources,source) {
+    $scope.addRemoveEventSource = function(sources, source) {
       var canAdd = 0;
-      angular.forEach(sources,function(value, key){
-        if(sources[key] === source){
-          sources.splice(key,1);
+      angular.forEach(sources, function(value, key) {
+        if (sources[key] === source) {
+          sources.splice(key, 1);
           canAdd = 1;
         }
       });
-      if(canAdd === 0){
+      if (canAdd === 0) {
         sources.push(source);
       }
     };
@@ -82,34 +125,36 @@ angular.module('schedules').controller('ScheduleController', [
     };
     /* remove event */
     $scope.remove = function(index) {
-      $scope.events.splice(index,1);
+      $scope.events.splice(index, 1);
     };
     /* Change View */
-    $scope.changeView = function(view,calendar) {        
-      uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
+    $scope.changeView = function(view, calendar) {
+      uiCalendarConfig.calendars[calendar].fullCalendar('changeView', view);
     };
     /* Change View */
     $scope.renderCalender = function(calendar) {
-      if(uiCalendarConfig.calendars[calendar]){
+      if (uiCalendarConfig.calendars[calendar]) {
         uiCalendarConfig.calendars[calendar].fullCalendar('render');
-      }      
+      }
     };
-     /* Render Tooltip */
-    $scope.eventRender = function( event, element, view ) { 
-        element.attr({'tooltip': event.title,
-                     'tooltip-append-to-body': true});
-        $compile(element)($scope);
+    /* Render Tooltip */
+    $scope.eventRender = function(event, element, view) {
+      element.attr({
+        'tooltip': event.title,
+        'tooltip-append-to-body': true
+      });
+      $compile(element)($scope);
     };
     /* config object */
     $scope.uiConfig = {
-      calendar:{
+      calendar: {
         height: 450,
         editable: true,
-        header:{
+        header: {
           left: 'prev',
           center: 'title',
           right: 'next'
-          //right: 'today prev,next'
+            //right: 'today prev,next'
         },
         eventClick: $scope.alertOnEventClick,
         eventDrop: $scope.alertOnDrop,
@@ -119,19 +164,23 @@ angular.module('schedules').controller('ScheduleController', [
     };
 
     $scope.changeLang = function() {
-      if($scope.changeTo === 'Hungarian'){
-        $scope.uiConfig.calendar.dayNames = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
+      if ($scope.changeTo === 'Hungarian') {
+        $scope.uiConfig.calendar.dayNames = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek",
+          "Szombat"
+        ];
         $scope.uiConfig.calendar.dayNamesShort = ["Vas", "Hét", "Kedd", "Sze", "Csüt", "Pén", "Szo"];
         $scope.changeTo = 'English';
       } else {
-        $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        $scope.uiConfig.calendar.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+          "Saturday"
+        ];
         $scope.uiConfig.calendar.dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
         $scope.changeTo = 'Hungarian';
       }
     };
     /* event sources array*/
-    //$scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];    
+    //$scope.eventSources = [$scope.events, $scope.eventSource, $scope.eventsF];
     $scope.eventSources = [$scope.events];
-    
+
   }
 ]);
